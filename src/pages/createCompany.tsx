2e04786617d25fa6ae36e79eb6381c0f718b646f
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-09-23 23:39:22 -0400

    add layout

--- src/pages/createCompany.tsx
+++ src/pages/createCompany.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
-
+import Layout from '@/components/layout';
 import CreateCompanyForm from '@/components/CreateCompany';
 
 const CreateCompany: FC = () => {
   return (
     <>
-      <CreateCompanyForm />
+      <Layout>
+        <CreateCompanyForm />
+      </Layout>
     </>
   );
 };
