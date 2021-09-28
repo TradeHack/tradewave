6f5845629e76382657cded0bab400220dbd09cee
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-09-28 00:44:22 -0400

    add company creation

---
+++ src/utils/createCompany.ts
@@ -0,0 +1,12 @@
+import Moralis from 'moralis';
+import { FormValues } from '@/components/CreateCompany';
+
+export const createCompany = async (values: FormValues, user: Moralis.User) => {
+  const Company = Moralis.Object.extend('Company');
+  const newCompany = new Company();
+  Object.entries(values).forEach(([key, value]) => {
+    newCompany.set(key, value);
+  });
+  newCompany.set('adminID', user.id);
+  await newCompany.save();
+};
